456d5f637891ae46cac5baeddd040daae1d39e0a
Zarowska <oksana.zarowska@gmail.com>
2024-03-15 01:47:27 +0100

    Implemented sending message functionality (test passed)

--- services/backend/backend-service/src/test/java/com/zarowska/cirkle/api/MessagesApiTest.java
+++ services/backend/backend-service/src/test/java/com/zarowska/cirkle/api/MessagesApiTest.java
@@ -1,18 +1,17 @@
 package com.zarowska.cirkle.api;
 
+import static org.junit.Assert.assertTrue;
+
 import com.zarowska.cirkle.AbstractTest;
 import com.zarowska.cirkle.api.model.*;
 import com.zarowska.cirkle.utils.TestUserContext;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
-
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class MessagesApiTest extends AbstractTest {
 
@@ -36,15 +35,14 @@ public class MessagesApiTest extends AbstractTest {
 
 	@Test
 	void testSendingMessageToUserById_Succeeds() {
-		 List<URI> imagesListURI = Stream.of("max_payne.png", "blazkovic.png")
-		 .map(it -> getFileFromResource("files/" + it))
-		 .map(imageResource ->
-		 bobContest.getApi().images().uploadImage(imageResource)).map(FileDto::getUrl)
-		 .toList();
-		 CreateMessageRequest request = CreateMessageRequest.builder().text("New message").images(imagesListURI).build();
-		 Optional<Message> newMessage =
-		 bobContest.getApi().messages().sendMessageToUserById(maxContext.getUserId(), request);
-		 assertTrue(newMessage.isPresent());
+		List<URI> imagesListURI = Stream.of("max_payne.png", "blazkovic.png")
+				.map(it -> getFileFromResource("files/" + it))
+				.map(imageResource -> bobContest.getApi().images().uploadImage(imageResource)).map(FileDto::getUrl)
+				.toList();
+		CreateMessageRequest request = CreateMessageRequest.builder().text("New message").images(imagesListURI).build();
+		Optional<Message> newMessage = bobContest.getApi().messages().sendMessageToUserById(maxContext.getUserId(),
+				request);
+		assertTrue(newMessage.isPresent());
 	};
 
 	@Disabled
