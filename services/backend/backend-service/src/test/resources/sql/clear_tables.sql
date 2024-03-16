e0e30b01b058bddbb9afa67579e2444702b19e1b
Zarowska <oksana.zarowska@gmail.com>
2024-03-16 22:18:56 +0100

    Changing the order of clearing the table before testing

--- services/backend/backend-service/src/test/resources/sql/clear_tables.sql
+++ services/backend/backend-service/src/test/resources/sql/clear_tables.sql
@@ -6,20 +6,17 @@ delete
 from post_image;
 delete
 from posts;
--- delete
--- from files_content;
--- delete
--- from files_info;
 delete
 from requests;
 delete
 from user_friends;
--- delete
--- from user_entity;
--- delete
--- from messages;
--- delete
--- from message_image;
---
-
-
+delete
+from message_image;
+delete
+from messages;
+delete
+from files_content;
+delete
+from files_info;
+delete
+from user_entity;
