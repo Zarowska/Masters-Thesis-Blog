2581fc3d920b9f4ef31f5b200c1ad4e6bf0ee665
Oksana Zarowska <oksana.zarowska@gmail.com>
2025-02-09 18:50:29 +0200

    added more tests

---
+++ gatling/gatling-maven-plugin-demo-java-main/src/test/java/RestApiSimulation500All.java
@@ -0,0 +1,27 @@
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import utils.Authentication;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static utils.RestHelper.*;
+
+public class RestApiSimulation500All extends Simulation {
+
+    // Scenario that exercises multiple REST endpoints
+//	public static ScenarioBuilder scn = scenario("REST_Api_Scenario").exec(getPostById).pause(1);
+//  public static ScenarioBuilder scn = scenario("REST_Api_Scenario").exec(getUsersPosts).pause(1);
+    public static ScenarioBuilder scn = scenario("REST_Api_Scenario")
+            .exec(getUsers).pause(1)
+            .exec(getPostById).pause(1)
+            .exec(getUsersPosts).pause(1);
+
+    static int concurrency = 500;
+
+    {
+        // We define injection steps for concurrency:
+        setUp(scn.injectOpen(
+                rampUsers(concurrency).during(10), // Ramp to 50 in 10s
+                constantUsersPerSec(concurrency).during(20)
+        )).protocols(Authentication.httpProtocol);
+    }
+}
